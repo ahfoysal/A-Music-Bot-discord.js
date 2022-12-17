1b4225e30fdd7cd382bcc8367e72da4f110d4f21
hosan30 <ahfoysal30@gmail.com>
2022-12-17 19:05:23 +0600

    asd
    asd

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const client = require("../index");
 client.on("ready", async () => {
   console.log(`${client.user.username} Is Online`);
   client.user.setActivity({
-    name: ` /help o_o "Made by Pewds"` ,
+    name: ` /help o_o Pewds👉👈` ,
     type: ActivityType.Playing,
   });
 
